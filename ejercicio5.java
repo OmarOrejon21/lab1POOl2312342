8ccaf41ef4e606cee9470dc066ecf422c79b3ecc
OmarOrejon21 <143566323+OmarOrejon21@users.noreply.github.com>
2023-09-02 20:38:54 -0500

    Add files via upload

---
+++ ejercicio5.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número del 1 al 7: ");
+        int numero = scanner.nextInt();
+
+        String diaSemana;
+
+        switch (numero) {
+            case 1:
+                diaSemana = "Lunes";
+                break;
+            case 2:
+                diaSemana = "Martes";
+                break;
+            case 3:
+                diaSemana = "Miércoles";
+                break;
+            case 4:
+                diaSemana = "Jueves";
+                break;
+            case 5:
+                diaSemana = "Viernes";
+                break;
+            case 6:
+                diaSemana = "Sábado";
+                break;
+            case 7:
+                diaSemana = "Domingo";
+                break;
+            default:
+                diaSemana = "Número fuera de rango";
+                break;
+        }
+
+        System.out.println("Día de la semana: " + diaSemana);
+
+        scanner.close();
+    }
+}
