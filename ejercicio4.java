2da27ecd94e447aaf4d0abf57d9710ce6ca2dd76
OmarOrejon21 <143566323+OmarOrejon21@users.noreply.github.com>
2023-09-02 20:34:35 -0500

    Add files via upload

---
+++ ejercicio4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el valor de numA: ");
+        int numA = scanner.nextInt();
+
+        System.out.print("Ingrese el valor de numB: ");
+        int numB = scanner.nextInt();
+
+        if ((numA > 0 && numB > 0) || (numA < 0 && numB < 0)) {
+            System.out.println("true");
+        }
+
+        scanner.close();
+    }
+}
